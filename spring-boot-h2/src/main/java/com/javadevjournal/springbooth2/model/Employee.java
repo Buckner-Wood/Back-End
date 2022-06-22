2dfee99ac68408963b0c00109ed7a31774864a51
Build-coder <59242259+Build-coder@users.noreply.github.com>
2022-06-22 11:21:04 -0400

    edited Employee file so that auto increment begins at 6 instead of 1 since data.sql already creates id's 1-5

--- spring-boot-h2/src/main/java/com/javadevjournal/springbooth2/model/Employee.java
+++ spring-boot-h2/src/main/java/com/javadevjournal/springbooth2/model/Employee.java
@@ -2,12 +2,25 @@ package com.javadevjournal.springbooth2.model;
 
 import javax.persistence.*;
 
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+
 @Entity
 @Table(name = "EMPLOYEE")
 public class Employee {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(generator = "sequence-generator")
+    @GenericGenerator(
+      name = "sequence-generator",
+      strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+      parameters = {
+        @Parameter(name = "sequence_name", value = "user_sequence"),
+        // start id at 6 b/c we initialize h2 db with 1-5 ids on startup
+        @Parameter(name = "initial_value", value = "6"),
+        @Parameter(name = "increment_size", value = "1")
+      }
+    )
     Integer id;
     String name;
     Double salary;
